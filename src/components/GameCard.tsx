86bd3b91690b6013fc1994b70b0b78ade0a25d49
ali9ahmad <temur.khan.dispatch@gmail.com>
2024-05-27 19:35:11 +0500

    customiza the theme to get darker grays

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card>
+    <Card height="400px">
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <HStack marginBottom={3}>

99e5a65535f90b3862afd05e8f45a824a2656f27
ali9ahmad <temur.khan.dispatch@gmail.com>
2024-05-27 20:01:10 +0500

    Updated the page title

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Box, Flex, Grid, GridItem, HStack, Show } from '@chakra-ui/react';
+import { Genre } from './hooks/useGenres';
+import { useState } from 'react';
+import { Platform } from './hooks/useGames';
 import NavBar from './components/NavBar';
 import GameGrid from './components/GameGrid';
 import GenreList from './components/GenreList';
-import { Genre } from './hooks/useGenres';
-import { useState } from 'react';
 import PlatformSelector from './components/PlatformSelector';
-import { Platform } from './hooks/useGames';
 import SortSelector from './components/SortSelector';
 import GameHeading from './components/GameHeading';
 
